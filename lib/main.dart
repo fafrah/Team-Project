2b62a77b4f3d7a99b3eb82ef31af948ef3055cfc
Farhiya <fafrah003@gmail.com>
2026-06-23 12:56:12 -0400

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Firebase Connected'))),
+      home: Scaffold(body: Center(child: Text('Firebase Initialized'))),
     );
   }
 }
