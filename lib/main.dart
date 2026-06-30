3363507fcc982e2a58e97014421d73c7eb33d0a3
Farhiya <fafrah003@gmail.com>
2026-06-30 12:08:56 -0400

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'theme/app_theme.dart';
+import 'screens/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MyApp());
+  runApp(const FindingRuby());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FindingRuby extends StatelessWidget {
+  const FindingRuby({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Finding Ruby',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Firebase Initialized'))),
+      theme: AppTheme.darkTheme,
+      home: const SplashScreen(),
     );
   }
 }
